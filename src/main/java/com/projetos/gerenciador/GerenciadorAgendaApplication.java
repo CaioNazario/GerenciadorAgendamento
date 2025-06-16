76efaf865e110331692f2a53ce59ac17e69863e4
caio <nazzariobarros@gmail.com>
2025-06-15 21:06:26 -0300

    ⚙️ Chore: estrutura inicial do projeto

---
+++ src/main/java/com/projetos/gerenciador/GerenciadorAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.projetos.despertagrana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespertagranaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespertagranaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetos/despertagrana/DespertagranaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.despertagrana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespertagranaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
