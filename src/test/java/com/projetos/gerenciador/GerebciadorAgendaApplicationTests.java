2e16e2df2ccc3ba4f4d19a7864424991540b0c00
caio <nazzariobarros@gmail.com>
2025-06-29 21:57:04 -0300

    feat: ✨ definindo a estrutura inicial do banco de dados para agendamentos

--- src/test/java/com/projetos/gerenciador/GerebciadorAgendaApplicationTests.java
+++ src/test/java/com/projetos/gerenciador/GerebciadorAgendaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DespertagranaApplicationTests {
+class GerebciadorAgendaApplicationTests {
 
 	@Test
 	void contextLoads() {
