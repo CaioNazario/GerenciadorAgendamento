1fe398ff0313b1a50113838004c027caa3f7b237
caio <nazzariobarros@gmail.com>
2025-07-20 19:24:45 -0300

    ✨feat(user): Add finByEmail
    Implements personalized way to find user.
    
    - Allows find user by email address.

--- src/main/java/com/projetos/gerenciador/user/UserRepository.java
+++ src/main/java/com/projetos/gerenciador/user/UserRepository.java
@@ -1,4 +1,10 @@
 package com.projetos.gerenciador.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<Object> findByEmail(String email);
 }
