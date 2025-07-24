f982988576aae1c3450e27ddcbce5b0beecae8f9
caio <nazzariobarros@gmail.com>
2025-07-23 21:40:58 -0300

    ♻️refactor: refactor createUser logic
    This commit refactor the createUser to get better logic using DTO's
    
    -new way to mapping user params, name, email, password

--- src/main/java/com/projetos/gerenciador/user/UserService.java
+++ src/main/java/com/projetos/gerenciador/user/UserService.java
@@ -1,36 +1,38 @@
 package com.projetos.gerenciador.user;
 
-import com.projetos.gerenciador.exception.ErrorCode;
+import com.projetos.gerenciador.exception.ErrCode;
 import com.projetos.gerenciador.exception.BusinessException;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserService {
-
+    
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User createUser(User user) {
+    public User createUser(CreateUserRequest createUserRequest) {
 
-        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-            throw new BusinessException(ErrorCode.EMAIL_ALREADY_EXISTS);
+        if (userRepository.findByEmail(createUserRequest.email()).isPresent()) {
+            throw new BusinessException(ErrCode.EMAIL_ALREADY_EXISTS);
         }
-        return userRepository.save(user);
+        User newUser = new User();
+        newUser.setUsername(createUserRequest.name());
+        newUser.setEmail(createUserRequest.email());
+        newUser.setPassword(createUserRequest.password());
+        return userRepository.save(newUser);
     }
 
     public void  deleteUser (Long id) {
 
         if (!userRepository.existsById(id)) {
-            throw new BusinessException(ErrorCode.USER_NOT_FOUND);
+            throw new BusinessException(ErrCode.USER_NOTFOUND);
         }
         userRepository.deleteById(id);
-
     }
 
 
 }
-
